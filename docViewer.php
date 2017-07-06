b74345591f0af7659492542772e536c04a50dc71
Justin Sorensen <sorensenjg@gmail.com>
2017-07-06 01:13:06 -0700

    Add shortcode

--- docViewer.php
+++ docViewer.php
@@ -24,7 +24,12 @@
   }
 
   // Display docViewer component
-  function docViewer($url, $name) {
+  function docViewer( $url, $name ) {
+
+    ob_start();
+
+    $url = $url;
+    $name = $name;
 
     $file = basename($url);
     $filename = preg_replace('/\\.[^.\\s]{3,4}$/', '', $file);
@@ -46,37 +51,116 @@
       $name = $filename;
     }
 
-    $docViewer = '<a href="#'. $filename .'-modal-'. $random_int .'" class="docViewer-open">'. $name .'</a>
+    ?>
 
-                  <div id="'. $filename .'-modal-'. $random_int .'" class="docViewer mfp-hide">
-                    <div class="docViewer-inner">
+      <a href="#<?php echo $filename; ?>-modal-<?php echo $random_int; ?>" class="docViewer-open"><?php echo $name; ?></a>
 
-                      <div id="docViewer-header">
-                        <div class="docViewer-header-inner left">
-                          <label>'. $name .'</label>
-                        </div>
-                        <div class="docViewer-header-inner right">
-                          <a href="'. $url .'" download><i class="fa fa-download" aria-hidden="true"></i></a>
-                          <a href="javascript:void(0)" id="docViewer-close"><i class="fa fa-times" aria-hidden="true"></i></a>
-                        </div>
-                      </div>
+      <div id="<?php echo $filename; ?>-modal-<?php echo $random_int; ?>" class="docViewer mfp-hide">
+        <div class="docViewer-inner">
 
-                      <div id="docViewer-body">
-                        '. $content .'
-                        <div id="docViewer-controls" class="'. $file_type .'">
-                          <div class="docViewer-controls-inner center">
-                            <input type="text" id="zoomVal" name="zoomVal" value="100%" />
-                            <div id="zoomOut"><i class="fa fa-search-minus" aria-hidden="true"></i></div>
-                            <div id="zoomIn"><i class="fa fa-search-plus" aria-hidden="true"></i></div>
-                            <div id="actualSize">Original Size</div>
-                            <div id="fitContainer">Fit on Screen</div>
-                          </div>
-                        </div>
-                      </div>
+          <div id="docViewer-header">
+            <div class="docViewer-header-inner left">
+              <label><?php echo $name; ?></label>
+            </div>
+            <div class="docViewer-header-inner right">
+              <a href="<?php echo $url; ?>" download><i class="fa fa-download" aria-hidden="true"></i></a>
+              <a href="javascript:void(0)" id="docViewer-close"><i class="fa fa-times" aria-hidden="true"></i></a>
+            </div>
+          </div>
 
-                    </div>
-                  </div>';
+          <div id="docViewer-body">
+            <?php echo $content; ?>
+            <div id="docViewer-controls" class="<?php echo $file_type; ?>">
+              <div class="docViewer-controls-inner center">
+                <input type="text" id="zoomVal" name="zoomVal" value="100%" />
+                <div id="zoomOut"><i class="fa fa-search-minus" aria-hidden="true"></i></div>
+                <div id="zoomIn"><i class="fa fa-search-plus" aria-hidden="true"></i></div>
+                <div id="actualSize">Original Size</div>
+                <div id="fitContainer">Fit on Screen</div>
+              </div>
+            </div>
+          </div>
 
-  	return $docViewer;
+        </div>
+      </div>
+
+    <?php
+
+    return ob_get_clean();
+
+    exit;
+
 
   }
+
+  // Display docViewer component via shortcode
+  function shortcode_docViewer( $atts ) {
+
+    ob_start();
+
+    $url = $atts['url'];
+    $name = $atts['name'];
+
+    $file = basename($url);
+    $filename = preg_replace('/\\.[^.\\s]{3,4}$/', '', $file);
+
+    $file_info = new SplFileInfo($file);
+    $file_type = $file_info->getExtension();
+
+    if( $file_type == 'jpg' || $file_type == 'png' || $file_type == 'gif' ) {
+      $content = '<img src="'. $url .'" />';
+    } else {
+      $content = '<iframe src="//drive.google.com/viewer?url='. $url .'&embedded=true"></iframe>';
+    }
+
+    $random_int = rand();
+
+    if( $name ) {
+      $name = $name;
+    } else {
+      $name = $filename;
+    }
+
+    ?>
+
+      <a href="#<?php echo $filename; ?>-modal-<?php echo $random_int; ?>" class="docViewer-open"><?php echo $name; ?></a>
+
+      <div id="<?php echo $filename; ?>-modal-<?php echo $random_int; ?>" class="docViewer mfp-hide">
+        <div class="docViewer-inner">
+
+          <div id="docViewer-header">
+            <div class="docViewer-header-inner left">
+              <label><?php echo $name; ?></label>
+            </div>
+            <div class="docViewer-header-inner right">
+              <a href="<?php echo $url; ?>" download><i class="fa fa-download" aria-hidden="true"></i></a>
+              <a href="javascript:void(0)" id="docViewer-close"><i class="fa fa-times" aria-hidden="true"></i></a>
+            </div>
+          </div>
+
+          <div id="docViewer-body">
+            <?php echo $content; ?>
+            <div id="docViewer-controls" class="<?php echo $file_type; ?>">
+              <div class="docViewer-controls-inner center">
+                <input type="text" id="zoomVal" name="zoomVal" value="100%" />
+                <div id="zoomOut"><i class="fa fa-search-minus" aria-hidden="true"></i></div>
+                <div id="zoomIn"><i class="fa fa-search-plus" aria-hidden="true"></i></div>
+                <div id="actualSize">Original Size</div>
+                <div id="fitContainer">Fit on Screen</div>
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </div>
+
+    <?php
+
+    return ob_get_clean();
+
+    exit;
+
+  }
+
+  // Expose shortcode
+  add_shortcode( 'docViewer', 'shortcode_docViewer');
